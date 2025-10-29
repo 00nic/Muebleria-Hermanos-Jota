ab532ac3895b6d00a704ce9822f53fb32396dd1b
nahhhu <nahucordero06@gmail.com>
2025-10-29 02:19:10 +0000

    comentarios

--- backend/src/utils/config.js
+++ backend/src/utils/config.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+//recordar crear archivo .env y agregar el url de la base de datos en DB_CONNECTION_STRING
 export const config = {
     databaseURL: process.env.DB_CONNECTION_STRING,
     port: process.env.PORT || 5000
