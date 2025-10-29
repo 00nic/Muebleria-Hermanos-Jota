e117a392f3e94d9686ba0be19375b0dca6cc3da8
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-10-29 17:07:17 -0300

    Fix: Product Schema

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,30 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
-    nombre: {
-        type: String,
-        required: true
-    },
-    descripcion: {
-        type: String
-    },
-    precio: {
-        type: Number,
-        required: true
-    },
-    stock: {
-        type: Number,
-        default: 0
-    },
-    imageUrl: {
-        type: String,
-        required: false
-    },
-    detalle: {
-        type: Object
-    }
+  nombre: {
+    type: String,
+    required: true,
+  },
+  descripcion: {
+    type: String,
+  },
+  precio: {
+    type: Number,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    default: 0,
+  },
+  imageUrl: {
+    type: String,
+    required: false,
+  },
+  detalle: {
+    type: Object,
+  },
+  destacado: {
+    type: Boolean,
+    default: false,
+  },
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
