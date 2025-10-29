c0a7371a5204fc8d1f9564ffc89bc018ce6429de
nahhhu <nahucordero06@gmail.com>
2025-10-29 02:42:43 +0000

    schema product creado

---
+++ backend/src/models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+    nombre: {
+        type: String,
+        required: true
+    },
+    descripcion: {
+        type: String
+    },
+    precio: {
+        type: Number,
+        required: true
+    },
+    stock: {
+        type: Number,
+        default: 0
+    },
+    imageUrl: {
+        type: String,
+        required: false
+    },
+    detalle: {
+        type: Object
+    }
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
