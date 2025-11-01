69dab5bf14d375231c8105bc5ca143eee6ffa54f
00nic <nicoenzogon@gmail.com>
2025-11-01 19:42:13 -0300

    funcionalidad eliminar prod desde front detail page

--- client/src/pages/ProductDetailPage.js
+++ client/src/pages/ProductDetailPage.js
@@ -8,7 +8,10 @@ export default function ProductDetailPage() {
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [productData, setProductData]= useState(null);
-    const {id} = useParams()
+    const {id} = useParams();
+    const [deleteError, setDeleteError] = useState(null);
+    const [deleteLoading, setDeleteLoading] = useState(false);
+    const navigate= useNavigate();
 
     useEffect(() => {
             const getProductById = async () => {
@@ -32,32 +35,59 @@ export default function ProductDetailPage() {
                     setLoading(false);
                 }
             }; getProductById();
-        }, [id]);
+    }, [id]);
 
-        if(loading){
-            return <p>Cargando datos del producto...</p>
+    const handleDelete = async () => {
+        if (!window.confirm("¿Seguro que quieres eliminar este producto?")) return;
+
+        setDeleteError(null);
+        setDeleteError(true);
+
+        try{
+            const response= await fetch(`/api/productos/${productData._id}`, {
+                method: 'DELETE',
+                headers: {'Content-Type': 'application/json',}
+            });
+            if (!response.ok){
+                throw new Error('No fue posible borrar el producto')
+            }
+            navigate('/productos')
+
+        }catch(error){
+            setDeleteError(error.message)
+        }finally{
+            setDeleteLoading(false)
         }
-        if(error){
-            return <p>No fue posible obtener los datos: {error.message}</p>
-        }
-        return(
+    };
+
+    if(loading){
+        return <p>Cargando datos del producto...</p>
+    }
+    if(error){
+        return <p>No fue posible obtener los datos: {error.message}</p>
+    }
+    return(
+        <div>
+            <Link to='/productos'>Volver al catálogo</Link>
             <div>
-                <Link to='/productos'>Volver al catálogo</Link>
-                <div>
-                    <img src={productData.imagenUrl} alt='imagen del producto'></img>
-                    <h3>{productData.nombre}</h3>
-                    <h4>{productData.descripcion}</h4>
-                    <p>{productData.precio}</p>
-                </div>
-                <div>
-                    {Object.entries(productData.detalle).map(([clave, valor]) => (
-                        <div key={clave}>
-                            <h2>{startCase(clave)}</h2>
-                            <p>{valor}</p>
-                        </div>
-                    ))}
-                </div>
-
+                <img src={productData.imagenUrl} alt='imagen del producto'></img>
+                <h3>{productData.nombre}</h3>
+                <h4>{productData.descripcion}</h4>
+                <p>{productData.precio}</p>
+                <button onClick={handleDelete} disabled={deleteLoading}>
+                    { loading ? "Eliminando.." : "Eliminar producto"}
+                </button>
+                {deleteError && <p style={{color: "red"}}>{deleteError}</p>}
             </div>
-        )
+            <div>
+                {Object.entries(productData.detalle).map(([clave, valor]) => (
+                    <div key={clave}>
+                        <h2>{startCase(clave)}</h2>
+                        <p>{valor}</p>
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
 }
