b23445113b96ffabc6543d873427b96a49a0ad28
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-11-01 22:07:30 -0300

    feat: add ContactForm component and update routing in App.js; refactor HeroBanner for consistency

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Navbar from "./components/NavBar";
 import ProductosPage from "./pages/ProductosPage";
+import ContactForm from "./components/ContactForm";
+// add import use state
 
 function App() {
   return (
@@ -10,12 +12,13 @@ function App() {
       <Navbar cart={[]} />
       <main>
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/productos' element={<ProductosPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/productos" element={<ProductosPage />} />
+          <Route path="/contacto" element={<ContactForm />} />
         </Routes>
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
--- client/src/components/home/HeroBanner.js
+++ client/src/components/home/HeroBanner.js
@@ -2,21 +2,23 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 const HeroBanner = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleNavigateToProducts = () => {
-        navigate('/productos')
-    }
+  const handleNavigateToProducts = () => {
+    navigate("/productos");
+  };
+  const handleNavigateToContact = () => {
+    navigate("/contacto");
+  };
 
-    return (
-        <div className="hero-banner">
-            <h1>Bienvenidos a Muebleria Hermanos Jota</h1>
-            <p>¡Renová tu hogar con estilo!</p>
-            <button onClick={handleNavigateToProducts}>
-                Ver productos
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="hero-banner">
+      <h1>Bienvenidos a Muebleria Hermanos Jota</h1>
+      <p>¡Renová tu hogar con estilo!</p>
+      <button onClick={handleNavigateToProducts}>Ver productos</button>
+      <button onClick={handleNavigateToContact}>Contacto</button>
+    </div>
+  );
+};
 
 export default HeroBanner;
