a438623065fe5dde0fb1b042a9b9d92b0545fa6a
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-11-25 12:55:09 -0300

    refactor: simplify CartPage component by utilizing useCart, but temporarily, its props drilling

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,14 +1,17 @@
 import Cart from "../components/Cart";
+import { useCart } from "../context/CartContext";
 
-const CartPage = ({ cart, addItem, removeItem, deleteItem }) => {
-  return (
-    <Cart
-      cart={cart}
-      addItem={addItem}
-      removeItem={removeItem}
-      deleteItem={deleteItem}
-    />
-  );
+const CartPage = () => {
+    const { cart, addItem, removeItem, deleteItem } = useCart();
+
+    return (
+        <Cart
+            cart={cart}
+            addItem={addItem}
+            removeItem={removeItem}
+            deleteItem={deleteItem}
+        />
+    );
 };
 
 export default CartPage;
