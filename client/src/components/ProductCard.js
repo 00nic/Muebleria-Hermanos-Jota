c7b10745aae91745a99bb7cced910b2a3cd7ed18
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-11-25 13:01:25 -0300

    refactor: improve formatting and readability in ProductCard component

--- client/src/components/ProductCard.js
+++ client/src/components/ProductCard.js
@@ -2,25 +2,24 @@ import { getImageUrl } from "../service/products";
 import { formatearPrecio } from "../utils/formatearPrecio";
 
 const ProductCard = ({ product, onClick }) => {
-  const imageUrl = getImageUrl(product.imagenUrl);
+    const imageUrl = getImageUrl(product.imagenUrl);
 
-  return (
-    <div className="product-card" onClick={onClick}>
-      <h3 className="product-name">{product.nombre}</h3>
-      {imageUrl && (
-        <img
-          className="product-image"
-          src={imageUrl}
-          alt={product.nombre}
-          width="200"
-        />
-      )}
-      <p className="product-description">{product.descripcion}</p>
-      <p className="product-price">
-        <strong>Precio: {formatearPrecio(product.precio)}</strong>
-      </p>
-    </div>
-  );
+    return (
+        <div className="product-card" onClick={onClick}>
+            <h3 className="product-name">{product.nombre}</h3>
+            {imageUrl && (
+                <img
+                    className="product-image"
+                    src={imageUrl}
+                    alt={product.nombre}
+                />
+            )}
+            <p className="product-description">{product.descripcion}</p>
+            <p className="product-price">
+                <strong>Precio: {formatearPrecio(product.precio)}</strong>
+            </p>
+        </div>
+    );
 };
 
 export default ProductCard;
