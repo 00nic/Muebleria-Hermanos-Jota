50e92d96933bff81030486e925dfd7c52c286eb0
Ibarra1812 <ibarraalvaro852@gmail.com>
2025-11-30 23:58:05 -0300

    Remove unused config import and frontendURL logging from logger middleware

--- backend/src/middlewares/logger.js
+++ backend/src/middlewares/logger.js
@@ -1,11 +1,9 @@
-import config from "../utils/config";
 const logger = (req, res, next) => {
     console.log(new Date().toString());
     console.log("Method:", req.method);
     console.log("Path:  ", req.path);
     console.log("Body:  ", req.body);
     console.log("-----");
-    console.log(config.frontendURL);
     next();
 };
 
